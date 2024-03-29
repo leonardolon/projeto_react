848470955cd05e300f433fef2102ec4824e24e7d
user <leonardo.lon@outlook.com>
2024-03-29 14:44:51 -0300

    aplicado os estilos

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,50 +1,85 @@
-import { useState } from "react"
+import {useState} from "react"
+
+import styles from './Formulario.module.css'
 
 const Formulario = () => {
-    let [peso, setPeso] = useState('');
-    let [altura, setAltura] = useState('');
+    const [altura, setAltura] = useState("");
+    const [peso, setPeso] = useState("");
+    const [mostraResultado, setmostraResultado] = useState("");
+    const [classificaTabela, setClassificaTabela] = useState(null);
 
-    const renderizaResultado = () => {
-        const calc = (peso / (altura * altura));
-
-        if (calc < 18,5){
-            return(
-                <p>Magreza</p>
-            )
+    const calculadoraImc = () => {
+        // const calculaImc =  peso / (altura * altura)
+        // const resultadoImc = calculaImc.toFixed(2);
+        // setmostraResultado(+resultadoImc)
+        if (altura === "" || peso == "") {
+            return;
         }
 
-        else if (calc < 25){
-            return(
-                <p>Normal</p>
-            )
-        }
+        const alturaFloat = parseFloat(altura);
+        const pesoFloat = parseFloat(peso);
 
-        else if (calc < 30){
-            return(
-                <p>Sobrepeso</p>
-            )
+        if (isNaN(alturaFloat) || isNaN(pesoFloat)) {
+            setClassificaTabela("Digite números válidos");
+            return;
         }
+        
+        const calculaImc = pesoFloat / (alturaFloat * alturaFloat)
+        const resultadoImc = calculaImc.toFixed(2);
+        setmostraResultado(+resultadoImc)
 
-        else if (calc < 40){
-            return(
-                <p>Obesidade</p>
+        if (resultadoImc <= 18.5) {
+            return setClassificaTabela(
+                <p>Você está a baixo do peso!</p>
             )
-        }
-
-        else{
-            return(
-                <p>Obesidade Grave</p>
+        } else if (resultadoImc >= 18.6 && resultadoImc <= 24.9 ) {
+            return setClassificaTabela (
+                <p>Parabéns! Seu peso está ideal</p>
+            )
+        } else if (resultadoImc >= 25 && resultadoImc <= 29.9){
+            return setClassificaTabela (
+                <p>Você está levemente acima do peso</p>
+            )
+        } else if (resultadoImc >= 30 && resultadoImc <= 34.9) {
+            return setClassificaTabela (
+                <p>Atenção! Obesidade grau I</p>
+            ) 
+        } else if (resultadoImc >= 35 && resultadoImc <= 39.9) {
+            return setClassificaTabela (
+                <p>Obesidade severa grau II</p>
+            )
+        } else if (resultadoImc >= 40) {
+            return setClassificaTabela (
+                <p>Obesidade mórbida grau III</p>
+            )
+        } else {
+            return (
+                <p>Digite um número válido</p>
             )
         }
     }
 
+    const reset = () => {    
+        setAltura("")
+        setPeso("")
+        setmostraResultado("")
+        setClassificaTabela(null)
+    }
+
     return (
-        <form>
-            <input type="number" placeholder="Sua altura" value={altura} onChange={evento => setAltura(evento.target.value) / 100} /> 
-            <input type="number" placeholder="Seu peso" value={peso} onChange={evento => setPeso(evento.target.value)}  /> 
-            {renderizaResultado()}
-        </form>
+        <div className="container">
+                <form className={styles.form}>
+                    <h1>Calculadora de IMC</h1>
+                    <p>Insira seus dados na calculadora para saber seu IMC:</p>
+                    <input className={styles.input} type="number" placeholder="Altura(M)" value={altura} onChange={evento => setAltura(parseFloat(evento.target.value))} />
+                    <input className={styles.input} type="number" placeholder="Peso(kg)" value={peso} onChange={evento => setPeso(parseFloat(evento.target.value))} />
+                    <button className={styles.button} onClick={calculadoraImc} type="button">Calcular</button>
+                    <button className={styles.button} onClick={reset} type="button">Resetar</button>
+                    <p>{mostraResultado}</p>
+                    {classificaTabela && <p>{classificaTabela}</p>}
+                </form>
+        </div>
     )
 }
 
-export default Formulario 
+export default Formulario
